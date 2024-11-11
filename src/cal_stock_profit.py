9bc3a8f9646da4f8933a64f23d890dd515491135
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2024-11-11 22:47:08 +0000

    improve

--- src/cal_stock_profit.py
+++ src/cal_stock_profit.py
@@ -1,36 +1,32 @@
-from typing import Union
 import matplotlib.pyplot as plt
 import pandas as pd
-from IPython.display import display
-from pathlib import Path
-from datetime import datetime
 
 plt.rcParams['axes.unicode_minus'] = False  # To show positive or negative symbol
 
-Pathlike = Union[Path | str]
 
+def cal_stock_profit(df: pd.DataFrame, stock_name: int) -> pd.Series:
+    """
+    Calculate and plot the yearly profit for a given stock.
 
-def cal_stock_profit(df: pd.DataFrame):
-    print('Your stocks include: ', df['stock'].unique())
-    name = input("Please enter the stock name: ")
-    stock_assigned = df.loc[df["stock"] == name]
-    print(stock_assigned)
-    display(stock_assigned.dropna())
-    profit = stock_assigned.dropna()['profit'].to_list()
-    print(f'Total profit in {name}: ' + 'NTD ' + str(sum(profit)))
-    stock_assigned['date_sold'] = pd.to_datetime(stock_assigned['date_sold']).dt.strftime(
-        '%Y/%m/%d').str.extract(r'(\d{4})')
-    profit_count = stock_assigned.groupby(['date_sold', 'stock'])['profit'].sum().unstack()
-    ax = profit_count.plot(kind='bar', stacked=True, figsize=(50, 6))
-    plt.xlabel('sale year')
+    :param df: DataFrame
+    :param stock_name: Integer code representing the stock to be analyzed
+    :return: pd.Series containing yearly profit for the specified stock
+    """
+    stock_assigned = df.loc[df["stock"] == stock_name]
+    stock_assigned['date_sold'] = pd.to_datetime(stock_assigned['date_sold'], errors='coerce')
+    # errors='coerce' ensures any invalid date formats are handled by assigning NaT (Not a Time).
+    # Extract the year
+    stock_assigned['year_sold'] = stock_assigned['date_sold'].dt.year
+    yearly_profit = stock_assigned.groupby('year_sold')['profit'].sum()
+    ax = yearly_profit.plot(kind='bar', stacked=True, figsize=(10, 6))
+    plt.xlabel('the year of sold')
     plt.ylabel("Profit(+/-)")
-    plt.title(f'Profit of {name}')
+    plt.title(f'Profit of the {stock_name}')
     plt.legend(title='stock', loc='upper left', bbox_to_anchor=(1, 1))
     plt.xticks(rotation=0)
     for container in ax.containers:
         ax.bar_label(container, fmt='%d', label_type='edge', fontsize=8)
 
     plt.show()
-
-# todo:1. 計算total profit. how calculate df[''] - df['']
-# todo: fix error
+    # Return calculated values for further analysis or testing
+    return yearly_profit
